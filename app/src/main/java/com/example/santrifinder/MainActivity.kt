23c12886eb5745b5be456d47e633400bea5b1cd7
Adi Firdaus <adifirdaussaputra@gmail.com>
2024-12-11 08:13:36 +0700

    Feat: Pembuatan UI/UX dasar

--- app/src/main/java/com/example/santrifinder/MainActivity.kt
+++ app/src/main/java/com/example/santrifinder/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.santrifinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+// DEKLARASI UNTUK BERPINDAH KE ACTIVITY TAMBAH DATA
+private lateinit var tombolTambahData: LinearLayout
+
+// DEKLARASI UNTUK BERPINDAH KE ACTIVITY DATA BARANG HILANG
+private lateinit var tombolLihatData: LinearLayout
+
+// DEKLARASI UNTUK BERPINDAH KE ACTIVITY BANTUAN
+private lateinit var tombolBantuan: LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +27,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // AKSI UNTUK BERPINDAH KE ACTIVITY TAMBAH DATA
+        tombolTambahData = findViewById(R.id.tombolTambahData)
+        tombolTambahData.setOnClickListener {
+            val intent = Intent(this, tambahData::class.java)
+            startActivity(intent)
+        }
+
+        // AKSI UNTUK BERPINDAH KE ACTIVITY DATA BARANG HILANG
+        tombolLihatData = findViewById(R.id.tombolLihatData)
+        tombolLihatData.setOnClickListener {
+            val intent = Intent(this, DataBarang::class.java)
+            startActivity(intent)
+        }
+        // AKSI UNTUK BERPINDAH KE ACTIVITY DATA BANTUAN
+        tombolBantuan = findViewById(R.id.tombolBantuan)
+        tombolBantuan.setOnClickListener {
+            val intent = Intent(this, DataBarang::class.java)
+            startActivity(intent)
+        }
+
     }
 }
